3847b311278345265cfcbb51ccce4db8840ae2a6
AjarnDang <torntan.j@gmail.com>
2023-11-16 15:09:11 +0700

    fix css, responsive and add flatpickr lib

---
+++ TestWebApplication1/Controllers/DeletedDataController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestWebApplication1.Controllers
+{
+    public class DeletedDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TestWebApplication1/Controllers/SettingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestWebApplication1.Controllers
+{
+    public class SettingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
