b2c718cbea055cc2a47e4c90e2fc8500e4908945
AjarnDang <torntan.j@gmail.com>
2023-11-24 14:34:58 +0700

    fix css and responsive layout

--- TestWebApplication1/wwwroot/js/site.js
+++ TestWebApplication1/wwwroot/js/site.js
@@ -6,5 +6,6 @@
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
+        $('#content').toggleClass('active');
     });
 });
